1100a72aa20c8877adfd394c73f83d2aa8777c50
javieraguedes <javierahermosa@yahoo.com>
2023-11-16 21:20:24 +0100

    first main app code

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+from flask import Flask, request, render_template,jsonify
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+
+def do_something(text1,text2):
+   text1 = text1.upper()
+   text2 = text2.upper()
+   combine = text1 + text2
+   return combine\
+
+@app.route('/')
+def home():
+    return render_template('index.html')\
+
+@app.route('/join', methods=['GET','POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    text2 = request.form['text2']
+    combine = do_something(text1,text2)
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True, host='0.0.0.0', port=5000)
